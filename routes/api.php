1594c302fb2b2ea963a9b0088a3a9d4c195ad204
Alvaro Breton <alvarolimadb@gmail.com>
2021-11-20 13:50:35 -0600

    Api key is added

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
 
 Route::post('/user','App\Http\Controllers\UsersController@store');
-
-Route::post('/products','App\Http\Controllers\ProductsController@store');
+/*Route::post('/products','App\Http\Controllers\ProductsController@store');
 Route::post('/create_order','App\Http\Controllers\OrdersController@store');
 Route::post('/order','App\Http\Controllers\OrdersController@index');
+Route::post('/report','App\Http\Controllers\ReportController@index');*/
+
+//http://127.0.0.1:8000/api/report?api_token=ji5CpRka1CYVehhq5e0PzFzk3kdYv4ylTecRgL5LjTvIKCMKv9d6mtvgm7pY
+
+/**
+ * Los siguiente endpoints necesitan API key
+ * Para generar un usuario se tiene que utilzar el endpoint: user
+ */
+
+Route::middleware('auth:api')->post('/products','App\Http\Controllers\ProductsController@store');
+Route::middleware('auth:api')->post('/create_order','App\Http\Controllers\OrdersController@store');
+Route::middleware('auth:api')->post('/order','App\Http\Controllers\OrdersController@index');
+Route::middleware('auth:api')->get('/report','App\Http\Controllers\ReportController@index');
 
